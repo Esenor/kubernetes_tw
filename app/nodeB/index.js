254870c63f6ea628cf76a918e6154fede345bbc5
Esenor <daviddieu80@gmail.com>
2020-02-28 16:52:11 +0100

    update node b response

--- app/nodeB/index.js
+++ app/nodeB/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 app.get('/', function (req, res) {
   res.json({
-    app: 'nodeB'
+    app: 'nodeB',
+    content: 'String data from container node-b in pod-node-b from service-node-b'
   })
 })
 app.listen(3000, function () {
